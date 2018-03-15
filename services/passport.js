94cd2acf5cde1a5b84cacefe895213028ed22482
unknown <anthonyhseo@gmail.com>
2018-03-14 19:40:52 -0700

    added React front end and finished stripe payments

--- services/passport.js
+++ services/passport.js
@@ -23,18 +23,13 @@ passport.use(
       callbackURL: "/auth/google/callback",
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then(existingUser => {
-        if (existingUser) {
-          // We already have a record with the given profile Id
-          done(null, existingUser);
-        } else {
-          // We don't have a user record with this ID, create new User.
-          new User({ googleId: profile.id })
-            .save()
-            .then(user => done(null, user));
-        }
-      });
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+      if (existingUser) {
+        return done(null, existingUser);
+      }
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
     }
   )
 );
